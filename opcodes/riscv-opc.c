746eaf428db00df693dd24234e26313988950637
Giuseppe Tagliavini <giuseppe.tagliavini@unibo.it>
2025-08-11 16:52:47 +0200

    Fixing fmulex.s.h and fmacex.s.h masks

--- opcodes/riscv-opc.c
+++ opcodes/riscv-opc.c
@@ -3463,8 +3463,10 @@ const struct riscv_opcode riscv_opcodes[] =
 {"fcvt.ah.h", 	"Xgap9",  "d,s",    	MATCH_FCVT_AH_H, 		MASK_FCVT_AH_H, 	match_opcode, 0 },
 
 /* Gap9 FP16,  Fp16, Fp16 => Fp32 */
-{"fmulex.s.h", "Xgap9", "d,s,t", 	MATCH_FMULEX_S_H, 		MASK_FMULEX_S_H, 	match_opcode, 0},
-{"fmacex.s.h", "Xgap9", "d,s,t", 	MATCH_FMACEX_S_H, 		MASK_FMACEX_S_H, 	match_opcode, 0},
+{"fmulex.s.h", "Xgap9", "d,s,t",   	MATCH_FMULEX_S_H | MASK_RM, 		MASK_FMULEX_S_H | MASK_RM, 	match_opcode, 0},
+{"fmulex.s.h", "Xgap9", "d,s,t,m", 	MATCH_FMULEX_S_H, 		MASK_FMULEX_S_H, 	match_opcode, 0},
+{"fmacex.s.h", "Xgap9", "d,s,t",  	MATCH_FMACEX_S_H | MASK_RM, 		MASK_FMACEX_S_H | MASK_RM, 	match_opcode, 0},
+{"fmacex.s.h", "Xgap9", "d,s,t,m", 	MATCH_FMACEX_S_H, 		MASK_FMACEX_S_H, 	match_opcode, 0},
 
 /* Gap9 FP16 ALT,  Fp16Alt, Fp16Alt => Fp32 */
 {"fmulex.s.ah", "Xgap9", "d,s,t",       MATCH_FMULEX_S_AH,              MASK_FMULEX_S_AH,       match_opcode, 0},
